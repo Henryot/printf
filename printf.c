26d5750d3211fc549133b4740e2b86f7f02f1963
Henry <adinoyiotaru@gmail.com>
2023-07-22 05:16:14 -0700

    4. Nothing in fine print is ever good news

--- printf.c
+++ printf.c
@@ -1,24 +1,29 @@
+#include "main.h"
 #include "printf_helpers.h"
 #include <stdarg.h>
-#include <stdio.h>
-#include "main.h"
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
 
 /**
- * _printf - Custom printf function
- * @format: The format string containing zero or more directives
+ * _printf - Custom printf function that formats and prints output.
+ *           Supports %c, %s, %d, %i, %u, %o, %x, %X, and %b conversions.
  *
- * Return: The number of characters printed
+ * @format: The format string containing the conversion specifiers.
+ * @...: The variadic arguments used for the conversion specifiers.
+ *
+ * Return: The number of characters printed.
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int printed_chars = 0;
-	int i;
-	print_func_t *func;
-
 	va_start(args, format);
 
-	for (i = 0; format[i] != '\0'; i++)
+	char buffer[BUFFER_SIZE];
+	int buffer_index = 0;
+	int printed_chars = 0;
+
+	for (int i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
 		{
@@ -26,22 +31,41 @@ int _printf(const char *format, ...)
 			if (format[i] == '\0')
 				break;
 
-			func = get_print_function(format[i]);
+			print_func_t *func = get_print_function(format[i]);
 			if (func != NULL)
-				printed_chars += func->func(args);
+			{
+				int count = func->func(args, buffer + buffer_index, BUFFER_SIZE - buffer_index);
+				buffer_index += count;
+				printed_chars += count;
+			}
 			else
 			{
-				putchar(format[i]);
-				printed_chars++;
+				if (buffer_index < BUFFER_SIZE - 1)
+				{
+					buffer[buffer_index++] = format[i];
+					printed_chars++;
+				}
 			}
 		}
 		else
 		{
-			putchar(format[i]);
-			printed_chars++;
+			if (buffer_index < BUFFER_SIZE - 1)
+			{
+				buffer[buffer_index++] = format[i];
+				printed_chars++;
+			}
+		}
+
+		if (buffer_index >= BUFFER_SIZE - 1)
+		{
+			write(1, buffer, buffer_index);
+			buffer_index = 0;
 		}
 	}
 
+	if (buffer_index > 0)
+		write(1, buffer, buffer_index);
+
 	va_end(args);
-	return (printed_chars);
+	return printed_chars;
 }
